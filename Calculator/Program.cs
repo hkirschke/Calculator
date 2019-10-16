1e1734d291d2ecafe44c1df0dab0a1a130832203
hkirschke <33073460+hkirschke@users.noreply.github.com>
2019-10-15 23:19:43 -0300

    Project add

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,11 +2,36 @@
 
 namespace Calculator
 {
-  class Program
+  static class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      if (args.Length < 3)
+      { 
+        Console.WriteLine("Numeros informados não são validos!");
+        Console.ReadKey();
+        return;
+      }
+      
+      switch (args[0])
+      {
+        case var op when op.Equals("+") || op.Equals("sum") || op.Equals("soma"):
+          Console.WriteLine(Sum.DoOperation(args[1], args[2]));
+          break;
+        case var op when op.Equals("-") || op.Equals("sub") || op.Equals("subtracao"):
+          Console.WriteLine(Subtraction.DoOperation(args[1], args[2]));
+          break;
+        case var op when op.Equals("/") || op.Equals("div") || op.Equals("divisao"):
+          Console.WriteLine(Division.DoOperation(args[1], args[2]));
+          break;
+        case var op when op.Equals("*") || op.Equals("mult") || op.Equals("multiplicacao") || op.Equals("x"):
+          Console.WriteLine(Multiplication.DoOperation(args[1], args[2]));
+          break;
+        default:
+          Console.WriteLine("Operação inválida");
+          Console.ReadKey();
+          break;
+      }
     }
   }
 }
